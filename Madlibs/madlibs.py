32fcac95db6efa34ccc921428bbc75a076a4d900
Gaby Nguyen <gabynyn@Gabys-MacBook-Pro.local>
2023-11-10 13:11:07 -0500

    Madlibs project done

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,15 @@
+name = input("Your name: ")
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famousName = input("Famous person: ")
+
+madlib = f"Hello my name is {name}. \
+    Programming is {adj} and I love to {verb1}. \
+        Have fun and {verb2} like {famousName}."
+        
+print(madlib)
+
+
+input("Press any key to exit.")
+exit()
